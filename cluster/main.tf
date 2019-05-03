92b01307a4094b7734281dd42bce5791d6515473
Samuel Daleo <lolamac@ssd.local>
2019-05-03 13:20:52 -0700

    Key, Config, Service file generations scripts. Changing instance generation in main.tf Creating modules

--- cluster/main.tf
+++ cluster/main.tf
@@ -23,8 +23,66 @@ resource "google_compute_instance" "controller" {
 		"tar -xvf etcd-v3.3.9-linux-amd64.tar.gz",
 		"sudo mv etcd-v3.3.9-linux-amd64/etcd* /usr/local/bin/",
 		"sudo mkdir -p /etc/etcd /var/lib/etcd",
-		"sudo cp ca.pem kubernetes-key.pem kubernetes.pem /etc/etcd/",
-		"export ETCD_NAME=$(hostname -s)",
+		"export ETCD_NAME="$(hostname -s)"",
+		"export INTERNAL_IP="$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)"",
+    "sudo mkdir -p /etc/kubernetes/config",
+    "wget -q --show-progress --https-only --timestamping "https://storage.googleapis.com/kubernetes-release/release/v1.12.0/bin/linux/amd64/kube-apiserver" "https://storage.googleapis.com/kubernetes-release/release/v1.12.0/bin/linux/amd64/kube-controller-manager" "https://storage.googleapis.com/kubernetes-release/release/v1.12.0/bin/linux/amd64/kube-scheduler" "https://storage.googleapis.com/kubernetes-release/release/v1.12.0/bin/linux/amd64/kubectl",
+    "chmod +x kube-apiserver kube-controller-manager kube-scheduler kubectl",
+    "sudo mv kube-apiserver kube-controller-manager kube-scheduler kubectl /usr/local/bin/",
+    "sudo mkdir -p /var/lib/kubernetes/",]
+		}
+  provisioner "file" {
+    source = "etcd.service/"
+    destination = "/etc/systemd/system"
+  }
+  provisioner "file" {
+    source      = "ca.pem"
+    destination = "/etc/etcd"
+   }
+  provisioner "file" {
+    source      = "kubernetes-key.pem"
+    destination = "/etc/etcd"
+  }
+  provisioner "file" {
+    source      = "kubernetes.pem"
+    destination = "/etc/etcd"
+  }
+  provisioner "file" {
+    source = "ca.pem"
+    destination = "/var/lib/kubernetes/"
+  }
+  provisioner "file" {
+    source = "ca-key.pem"
+    destination = "/var/lib/kubernetes/"
+  }
+  provisioner "file" {
+    source = "kubernetes-key.pem"
+    destination = "/var/lib/kubernetes/"
+  }
+  provisioner "file" {
+    source = "kubernetes.pem"
+    destination = "/var/lib/kubernetes/"
+  }
+  provisioner "file" {
+    source = "service-account-key.pem"
+    destination = "/var/lib/kubernetes/"
+  }
+  provisioner "file" {
+    source = "service-account.pem"
+    destination = "/var/lib/kubernetes/"
+  }
+  provisioner "file" {
+    source = "encryption-config.yaml/"
+    destination = "/var/lib/kubernetes/"
+  }
+  provisioner "file" {
+    source = "kube-apiserver.service"
+    destination = "/etc/systemd/system/"
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo systemctl daemon-reload","sudo systemctl enable etcd","sudo systemctl start etcd"]
+  }
+
 }
 
 resource "google_compute_instance" "controller" {
@@ -46,6 +104,34 @@ resource "google_compute_instance" "controller" {
     scopes = ["compute-rw","storage-ro","service-management","service-control","logging-write","monitoring"]
   }
   can_ip_forward = true
+  provisioner "remote-exec" {
+  inline = [
+    "wget -q --show-progress --https-only --timestamping "https://github.com/coreos/etcd/releases/download/v3.3.9/etcd-v3.3.9-linux-amd64.tar.gz"",
+    "tar -xvf etcd-v3.3.9-linux-amd64.tar.gz",
+    "sudo mv etcd-v3.3.9-linux-amd64/etcd* /usr/local/bin/",
+    "sudo mkdir -p /etc/etcd /var/lib/etcd",
+    "export ETCD_NAME="$(hostname -s)"",
+    "export INTERNAL_IP="$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/1/ip)""]
+    }
+  provisioner "file" {
+    source = "etcd.service/"
+    destination = "/etc/systemd/system"
+  }
+  provisioner "file" {
+    source      = "ca.pem"
+    destination = "/etc/etcd"
+   }
+  provisioner "file" {
+    source      = "kubernetes-key.pem"
+    destination = "/etc/etcd"
+  }
+  provisioner "file" {
+    source      = "kubernetes.pem"
+    destination = "/etc/etcd"
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo systemctl daemon-reload","sudo systemctl enable etcd","sudo systemctl start etcd"]
+  }
 }
 
 resource "google_compute_instance" "controller" {
@@ -140,6 +226,7 @@ resource "google_compute_instance" "worker" {
   }
   can_ip_forward = true
 }
+
 resource "google_compute_network" "vpc_network" {
   name = "kubernetes-net"
   auto_create_subnetworks = false
@@ -180,8 +267,6 @@ resource "google_compute_firewall" "kuberenetes-external" {
 }
 
 #@TODO:
-# Configure .ssh keys for instances
-
 # Need to reserve IP for External Load balancer?
 #resource "google_compute_address" "external_address" {
 #  name         = "external_IP"
